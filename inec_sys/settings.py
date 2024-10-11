cc74c65dd9eeb0da63c078bd8774f9130c1588a2
BlacAc3 <aceezeala@gmail.com>
2024-10-11 16:30:38 +0100

    postgres integration

--- inec_sys/settings.py
+++ inec_sys/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+env=environ.Env()
+environ.Env.read_env()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,6 +100,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+DATABASES = {
+    "default":dj_database_url.parse(env("DATABASE_URL"))
 }
 
 
