6a39be62c89fbf823b011c123902b46d2d4ea7a3
BlacAc3 <aceezeala@gmail.com>
2024-10-11 16:16:58 +0100

    fix vercel setup error

--- inec_sys/settings.py
+++ inec_sys/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oq!u)m7)#z&wu@kg)5q$)ryab=i%fnjf4=)o-f*(j)k02amlct'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["", "*"]
 
